b439c725fcf1c47ce86c0a00080c35e4ab5ab62b
nisa <khoirunnisa.nr03@gmail.com>
2022-08-05 04:17:44 +0700

    [fix] mainTitle

--- resources/views/landing/optionalContents/index.blade.php
+++ resources/views/landing/optionalContents/index.blade.php
@@ -7,57 +7,74 @@
 @endsection
 
 @section('page-content')
-<x-card.layout >
-    <x-slot name="header">
-        <h3 class="text-900 mb-0" data-anchor>Optional Content Management</h3>
-    </x-slot>
+    <x-card.layout mainTitle="Optional Menyu" subTitle="Optional Content Management">
+        <x-slot name="header">
+            <h3 class="text-900 mb-0" data-anchor>Optional Content Management</h3>
+        </x-slot>
 
-    <x-slot name="body">
-        <table class="table table-striped datatables-target-exec">
-            <thead>
-                <th>No</th>
-                <th>Title</th>
-                <th>Description</th>
-                <th>Target</th>
-                <th>Menu</th>
-                <th>Default Value</th>
-                <th>Action</th>
-            </thead>
-            <tbody>
-            </tbody>
-        </table>
-    </x-slot>
-</x-card.layout>
+        <x-slot name="body">
+            <table class="table table-striped datatables-target-exec">
+                <thead>
+                    <th>No</th>
+                    <th>Title</th>
+                    <th>Description</th>
+                    <th>Target</th>
+                    <th>Menu</th>
+                    <th>Default Value</th>
+                    <th>Action</th>
+                </thead>
+                <tbody>
+                </tbody>
+            </table>
+        </x-slot>
+    </x-card.layout>
 @endsection
 
 @section('footer-content')
-<script src="{{asset('assets/plugins/datatable/js/jquery.dataTables.min.js')}}"></script>
-<script src="{{asset('assets/plugins/datatable/js/dataTables.bootstrap5.min.js')}}"></script>
-<script src="{{asset('assets/js/bootstrap.bundle.min.js')}}"></script>
-<script>
-    $(document).ready(() => {
-        var table = $('.datatables-target-exec').DataTable({
-        processing: true,
-        serverSide: true,
-        ajax: "{{ route('optional.content.index') }}",
-        columns: [
-            {data: 'DT_RowIndex', name: 'DT_RowIndex',
-                sortable: false,
-                orderable: false,
-                searchable: false},
-            {data: 'title', name: 'title'},
-            {data: 'description', name: 'description'},
-            {data: 'target', name: 'target'},
-            {data: 'menu', name: 'menu'},
-            {data: 'default', name: 'default'},
-            {
-                data: 'action',
-                name: 'action',
-                orderable: false,
-                searchable: false
-            },
-        ]
-    });
-    })
-</script>
+    <script src="{{ asset('assets/plugins/datatable/js/jquery.dataTables.min.js') }}"></script>
+    <script src="{{ asset('assets/plugins/datatable/js/dataTables.bootstrap5.min.js') }}"></script>
+    <script src="{{ asset('assets/js/bootstrap.bundle.min.js') }}"></script>
+    <script>
+        $(document).ready(() => {
+            var table = $('.datatables-target-exec').DataTable({
+                processing: true,
+                serverSide: true,
+                ajax: "{{ route('optional.content.index') }}",
+                columns: [{
+                        data: 'DT_RowIndex',
+                        name: 'DT_RowIndex',
+                        sortable: false,
+                        orderable: false,
+                        searchable: false
+                    },
+                    {
+                        data: 'title',
+                        name: 'title'
+                    },
+                    {
+                        data: 'description',
+                        name: 'description'
+                    },
+                    {
+                        data: 'target',
+                        name: 'target'
+                    },
+                    {
+                        data: 'menu',
+                        name: 'menu'
+                    },
+                    {
+                        data: 'default',
+                        name: 'default'
+                    },
+                    {
+                        data: 'action',
+                        name: 'action',
+                        orderable: false,
+                        searchable: false
+                    },
+                ]
+            });
+        })
+    </script>
 @endsection
