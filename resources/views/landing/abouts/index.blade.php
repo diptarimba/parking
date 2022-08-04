15c1ff8eb416be5df247a339d9cced7c1f426288
nisa <khoirunnisa.nr03@gmail.com>
2022-08-05 04:16:28 +0700

    [fix] subTitle

--- resources/views/landing/abouts/index.blade.php
+++ resources/views/landing/abouts/index.blade.php
@@ -7,51 +7,59 @@
 @endsection
 
 @section('page-content')
-<x-card.layout >
-    <x-slot name="header">
-        <x-card.h-buat url="{{route('about.create')}}" title="About Management"/>
-    </x-slot>
+    <x-card.layout mainTitle="About" subTitle="About Management">
+        <x-slot name="header">
+            <x-card.h-buat url="{{ route('about.create') }}" title="About Management" />
+        </x-slot>
 
-    <x-slot name="body">
-        <table class="table table-striped datatables-target-exec">
-            <thead>
-                <th>No</th>
-                <th>Title</th>
-                <th>Description</th>
-                <th>Action</th>
-            </thead>
-            <tbody>
-            </tbody>
-        </table>
-    </x-slot>
-</x-card.layout>
+        <x-slot name="body">
+            <table class="table table-striped datatables-target-exec">
+                <thead>
+                    <th>No</th>
+                    <th>Title</th>
+                    <th>Description</th>
+                    <th>Action</th>
+                </thead>
+                <tbody>
+                </tbody>
+            </table>
+        </x-slot>
+    </x-card.layout>
 @endsection
 
 @section('footer-content')
-<script src="{{asset('assets/plugins/datatable/js/jquery.dataTables.min.js')}}"></script>
-<script src="{{asset('assets/plugins/datatable/js/dataTables.bootstrap5.min.js')}}"></script>
-<script src="{{asset('assets/js/bootstrap.bundle.min.js')}}"></script>
-<script>
-    $(document).ready(() => {
-        var table = $('.datatables-target-exec').DataTable({
-        processing: true,
-        serverSide: true,
-        ajax: "{{ route('about.index') }}",
-        columns: [
-            {data: 'DT_RowIndex', name: 'DT_RowIndex',
-                sortable: false,
-                orderable: false,
-                searchable: false},
-            {data: 'title', name: 'title'},
-            {data: 'description', name: 'description'},
-            {
-                data: 'action',
-                name: 'action',
-                orderable: false,
-                searchable: false
-            },
-        ]
-    });
-    })
-</script>
+    <script src="{{ asset('assets/plugins/datatable/js/jquery.dataTables.min.js') }}"></script>
+    <script src="{{ asset('assets/plugins/datatable/js/dataTables.bootstrap5.min.js') }}"></script>
+    <script src="{{ asset('assets/js/bootstrap.bundle.min.js') }}"></script>
+    <script>
+        $(document).ready(() => {
+            var table = $('.datatables-target-exec').DataTable({
+                processing: true,
+                serverSide: true,
+                ajax: "{{ route('about.index') }}",
+                columns: [{
+                        data: 'DT_RowIndex',
+                        name: 'DT_RowIndex',
+                        sortable: false,
+                        orderable: false,
+                        searchable: false
+                    },
+                    {
+                        data: 'title',
+                        name: 'title'
+                    },
+                    {
+                        data: 'description',
+                        name: 'description'
+                    },
+                    {
+                        data: 'action',
+                        name: 'action',
+                        orderable: false,
+                        searchable: false
+                    },
+                ]
+            });
+        })
+    </script>
 @endsection
