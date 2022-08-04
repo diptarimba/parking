224d16e087689ed2855d3399b519b92e4671d446
nisa <khoirunnisa.nr03@gmail.com>
2022-08-05 04:15:06 +0700

    [fix] subTitle

--- resources/views/components/card/layout.blade.php
+++ resources/views/components/card/layout.blade.php
@@ -2,13 +2,14 @@
     <div class="page-content">
         <!--breadcrumb-->
         <div class="page-breadcrumb d-none d-sm-flex align-items-center mb-3">
-            <div class="breadcrumb-title pe-3">{{$mainTitle ?? 'Kosong'}}</div>
+            <div class="breadcrumb-title pe-3">{{ $mainTitle ?? 'Kosong' }}</div>
             <div class="ps-3">
                 <nav aria-label="breadcrumb">
                     <ol class="breadcrumb mb-0 p-0">
                         <li class="breadcrumb-item"><a href="javascript:;"><i class="bx bx-home-alt"></i></a>
                         </li>
-                        <li class="breadcrumb-item active" aria-current="page">{{$subTitle ?? 'Kosong'}}</li>
+                        <li class="breadcrumb-item active" aria-current="page">
+                            {{ $subTitle ?? 'Kosong' }}</li>
                     </ol>
                 </nav>
             </div>
@@ -17,9 +18,9 @@
         <!--end breadcrumb-->
         <div class="card">
             <div class="card-body">
-                {{$header}}
-                <hr/>
-                {{$body}}
+                {{ $header }}
+                <hr />
+                {{ $body }}
             </div>
         </div>
     </div>
