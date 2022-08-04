9970032b3582039b5f540f230a2ffb5ecd8a0b43
nisa <khoirunnisa.nr03@gmail.com>
2022-08-05 04:18:48 +0700

    [fix] mainTitle

--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -7,53 +7,70 @@
 @endsection
 
 @section('page-content')
-<x-card.layout >
-    <x-slot name="header">
-        <x-card.h-buat url="{{route('user.create')}}" title="User Management"/>
-    </x-slot>
+    <x-card.layout mainTitle="User" subTitle="User Management">
+        <x-slot name="header">
+            <x-card.h-buat url="{{ route('user.create') }}" title="User Management" />
+        </x-slot>
 
-    <x-slot name="body">
-        <table class="table datatables-target-exec table-striped">
-            <thead>
-                <th>No</th>
-                <th>Name</th>
-                <th>Email</th>
-                <th>Phone</th>
-                <th>Role</th>
-                <th>Action</th>
-            </thead>
-            <tbody>
+        <x-slot name="body">
+            <table class="table datatables-target-exec table-striped">
+                <thead>
+                    <th>No</th>
+                    <th>Name</th>
+                    <th>Email</th>
+                    <th>Phone</th>
+                    <th>Role</th>
+                    <th>Action</th>
+                </thead>
+                <tbody>
 
-            </tbody>
-        </table>
-    </x-slot>
-</x-card.layout>
+                </tbody>
+            </table>
+        </x-slot>
+    </x-card.layout>
 @endsection
 
 @section('footer-content')
-<script src="{{asset('assets/plugins/datatable/js/jquery.dataTables.min.js')}}"></script>
-<script src="{{asset('assets/plugins/datatable/js/dataTables.bootstrap5.min.js')}}"></script>
-<script src="{{asset('assets/js/bootstrap.bundle.min.js')}}"></script>
-<script>
-    $(document).ready(() => {
-        var table = $('.datatables-target-exec').DataTable({
-        processing: true,
-        serverSide: true,
-        ajax: "{{ route('user.index') }}",
-        columns: [
-            {data: 'DT_RowIndex', name: 'DT_RowIndex', sortable: false, orderable:false, searchable:false},
-            {data: 'name', name: 'name'},
-            {data: 'email', name: 'email'},
-            {data: 'phone', name: 'phone'},
-            {data: 'user_role.name', name: 'user_role.name'},
-            {
-                data: 'action',
-                name: 'action',
-                orderable: false,
-                searchable: false
-            },
-        ]
-    });
-    })
-</script>
+    <script src="{{ asset('assets/plugins/datatable/js/jquery.dataTables.min.js') }}"></script>
+    <script src="{{ asset('assets/plugins/datatable/js/dataTables.bootstrap5.min.js') }}"></script>
+    <script src="{{ asset('assets/js/bootstrap.bundle.min.js') }}"></script>
+    <script>
+        $(document).ready(() => {
+            var table = $('.datatables-target-exec').DataTable({
+                processing: true,
+                serverSide: true,
+                ajax: "{{ route('user.index') }}",
+                columns: [{
+                        data: 'DT_RowIndex',
+                        name: 'DT_RowIndex',
+                        sortable: false,
+                        orderable: false,
+                        searchable: false
+                    },
+                    {
+                        data: 'name',
+                        name: 'name'
+                    },
+                    {
+                        data: 'email',
+                        name: 'email'
+                    },
+                    {
+                        data: 'phone',
+                        name: 'phone'
+                    },
+                    {
+                        data: 'user_role.name',
+                        name: 'user_role.name'
+                    },
+                    {
+                        data: 'action',
+                        name: 'action',
+                        orderable: false,
+                        searchable: false
+                    },
+                ]
+            });
+        })
+    </script>
 @endsection
