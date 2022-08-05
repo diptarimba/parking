738599024eafb30bb74ac5741fed6d9ac6708a9f
nisa <khoirunnisa.nr03@gmail.com>
2022-08-05 23:01:14 +0700

    [fix] mainTitle

--- resources/views/userRoles/create-edit.blade.php
+++ resources/views/userRoles/create-edit.blade.php
@@ -7,23 +7,22 @@
 @endsection
 
 @section('page-content')
-<x-card.layout >
-    <x-slot name="header">
-        {{ request()->routeIs('roles.create') ? 'Create User Role' : 'Update User Role' }}
-    </x-slot>
-    <x-slot name="body">
-        <form id="form"
-            action="{{ request()->routeIs('roles.create') ? route('roles.store'): route('roles.update', @$userRole->id) }}"
-            class="theme-form mega-form" method="post"
-            enctype="multipart/form-data">
-            @csrf
-            <x-forms.put-method />
-            <x-forms.input label="Name" name="name" :value="@$userRole->name"/>
-        </form>
-        <button form="form" class="btn btn-primary btn-pill">Submit</button>
-        <x-action.cancel />
-    </x-slot>
-</x-card.layout>
+    <x-card.layout mainTitle="User Role" subTitle="User Role Management">
+        <x-slot name="header">
+            {{ request()->routeIs('roles.create') ? 'Create User Role' : 'Update User Role' }}
+        </x-slot>
+        <x-slot name="body">
+            <form id="form"
+                action="{{ request()->routeIs('roles.create') ? route('roles.store') : route('roles.update', @$userRole->id) }}"
+                class="theme-form mega-form" method="post" enctype="multipart/form-data">
+                @csrf
+                <x-forms.put-method />
+                <x-forms.input label="Name" name="name" :value="@$userRole->name" />
+            </form>
+            <button form="form" class="btn btn-primary btn-pill">Submit</button>
+            <x-action.cancel />
+        </x-slot>
+    </x-card.layout>
 @endsection
 
 @section('footer-content')
