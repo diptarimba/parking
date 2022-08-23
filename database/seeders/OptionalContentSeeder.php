4cfb8098f9aa6af199d76576969dfee5ca179b55
dipta harimbawa <vipalkuda@gmail.com>
2022-08-23 11:27:41 +0700

    [fix] seeder

--- database/seeders/OptionalContentSeeder.php
+++ database/seeders/OptionalContentSeeder.php
@@ -6,6 +6,7 @@ use Carbon\Carbon;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Str;
 
 class OptionalContentSeeder extends Seeder
 {
@@ -20,37 +21,37 @@ class OptionalContentSeeder extends Seeder
             [
                 'title' => 'About Application',
                 'description' => 'Lorem ipsum dolor sit amet',
-                'target' => substr(md5(Carbon::now()->subMinutes(rand(1, 55))), 0, 5),
+                'target' => Str::random(10),
                 'menu' => 'ABOUT',
                 'default' => 'About Application'
             ],[
                 'title' => 'User Activity',
                 'description' => 'Lorem ipsum dolor sit amet',
-                'target' => substr(md5(Carbon::now()->subMinutes(rand(1, 55))), 0, 5),
+                'target' => Str::random(10),
                 'menu' => 'ACTIVITY',
                 'default' => 'User Activity'
             ],[
                 'title' => 'Location',
                 'description' => 'Lorem ipsum dolor sit amet',
-                'target' => substr(md5(Carbon::now()->subMinutes(rand(1, 55))), 0, 5),
+                'target' => Str::random(10),
                 'menu' => 'LOCATION',
                 'default' => 'Location'
             ],[
                 'title' => 'Feedback',
                 'description' => 'Lorem ipsum dolor sit amet',
-                'target' => substr(md5(Carbon::now()->subMinutes(rand(1, 55))), 0, 5),
+                'target' => Str::random(10),
                 'menu' => 'FEEDBACK',
                 'default' => 'Feedback'
             ],[
                 'title' => 'Contact Us',
                 'description' => 'Lorem ipsum dolor sit amet',
-                'target' => substr(md5(Carbon::now()->subMinutes(rand(1, 55))), 0, 5),
+                'target' => Str::random(10),
                 'menu' => 'CONTACT',
                 'default' => 'Contact Us'
             ],[
                 'title' => 'Faq',
                 'description' => 'Lorem ipsum dolor sit amet',
-                'target' => substr(md5(Carbon::now()->subMinutes(rand(1, 55))), 0, 5),
+                'target' => Str::random(10),
                 'menu' => 'FAQ',
                 'default' => 'Faq'
             ],
